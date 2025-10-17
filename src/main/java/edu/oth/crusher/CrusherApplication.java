1a3d8f87b37bfbae72d45c217a8d404bb6e9b1ff
hannes <hannes.weber@st.oth-regensburg.de>
2025-10-17 16:40:18 +0200

    initial setup

---
+++ src/main/java/edu/oth/crusher/CrusherApplication.java
@@ -0,0 +1,13 @@
+package edu.oth.crusher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrusherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrusherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/oth/crusher/CrusherApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.oth.crusher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrusherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
