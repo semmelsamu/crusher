7f2e997913c14867d385d3ce8545131c23f1fdd8
hannes <hannes.weber@st.oth-regensburg.de>
2025-11-22 14:21:25 +0100

    feat: add gym entity and gym repository

--- src/main/java/de/othr/crusher/controller/GymController.java
+++ src/main/java/de/othr/crusher/controller/GymController.java
@@ -1,8 +1,11 @@
 package de.othr.crusher.controller;
 
+import de.othr.crusher.model.Gym;
+import de.othr.crusher.repository.GymRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -10,8 +13,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin/gyms")
 public class GymController {
 
+    private final GymRepository gymRepository;
+
+    public GymController(GymRepository gymRepository) {
+        this.gymRepository = gymRepository;
+    }
+
     @GetMapping
-    public String showUserList(Model model) {
+    public String showAllGyms(Model model) {
+
+        model.addAttribute("gyms", gymRepository.findAll());
+
         return "pages/admin/gyms";
+    }
+
+    @GetMapping("/{id}")
+    public String showGymForId(@PathVariable("id") long id, Model model) {
+
+        Gym gym = gymRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Gym not found"));
+
+        model.addAttribute("gym", gym);
+
+        return "pages/admin/gym";
     }
 }
---
+++ src/main/java/de/othr/crusher/model/Gym.java
@@ -0,0 +1,76 @@
+package de.othr.crusher.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "gyms")
+public class Gym {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String city;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Column(nullable = false)
+    private String email;
+
+    public Gym() {}
+
+    public Gym(String name, String street, String city, String email) {
+        this.name = name;
+        this.street = street;
+        this.city = city;
+        this.email = email;
+    }
+
+    // ---- Getters / Setters ----
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
