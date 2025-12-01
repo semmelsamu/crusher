036a41022dd2d82686d9b59d78e8ddffefd79644
hannes <hannes.weber@st.oth-regensburg.de>
2025-12-01 16:36:31 +0100

    chore: cleaned up imports

--- src/main/java/de/othr/crusher/controller/GymController.java
+++ src/main/java/de/othr/crusher/controller/GymController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
 import java.util.List;
 import java.util.Map;
 
