7354d9fef05e11ecee84bc44945797d6c4d0d84e
Samuel Kroiß <mail@samuelkroiss.de>
2025-12-08 16:21:34 +0100

    feat(ui): add basic generic toast component

--- src/main/java/de/othr/crusher/HomeController.java
+++ src/main/java/de/othr/crusher/HomeController.java
@@ -1,16 +1,28 @@
 package de.othr.crusher;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
     @RequestMapping(value="/", method= RequestMethod.GET)
-    public String readersBooks() {
+    public String readersBooks(RedirectAttributes redirectAttributes) {
         return "pages/home";
+    }
+
+    @RequestMapping(value="/te", method= RequestMethod.GET)
+    public String te(RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("toast", Map.of(
+            "message", "Hello!"
+        ));
+        
+        return "redirect:/";
     }
     
     @RequestMapping(value="/components", method= RequestMethod.GET)
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -7,14 +7,3 @@ window.closeDialog = (id) => {
     const dialog = document.getElementById(id);
     dialog.close();
 };
-
-// Auto-dismiss toast notifications
-document.addEventListener("DOMContentLoaded", () => {
-    const toast = document.querySelector(".toast-success");
-    if (toast) {
-        setTimeout(() => {
-            toast.style.animation = "toast-slide-in 0.3s ease-out reverse";
-            setTimeout(() => toast.remove(), 300);
-        }, 3000);
-    }
-});
