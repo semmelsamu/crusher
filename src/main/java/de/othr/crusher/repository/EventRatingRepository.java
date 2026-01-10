6a51efee425c41f8209ca98a0d8e3352d697d0ce
Carl Czarnetzki <carl.czarnetzki@gmail.com>
2026-01-10 22:29:53 +0100

    feat(events): add event comment and rating models/controllers

---
+++ src/main/java/de/othr/crusher/repository/EventRatingRepository.java
@@ -0,0 +1,26 @@
+package de.othr.crusher.repository;
+
+import de.othr.crusher.model.EventRatingEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EventRatingRepository extends JpaRepository<EventRatingEntity, Long> {
+    /**
+     * Finds a rating for a given user and event, if it exists.
+     *
+     * @param userId the ID of the user
+     * @param eventId the ID of the event
+     * @return the rating entity, if found
+     */
+    Optional<EventRatingEntity> findByUserIdAndEventId(Long userId, Long eventId);
+
+    /**
+     * Finds all ratings for a given event.
+     *
+     * @param eventId the ID of the event
+     * @return list of ratings for the event
+     */
+    List<EventRatingEntity> findByEventId(Long eventId);
+}
